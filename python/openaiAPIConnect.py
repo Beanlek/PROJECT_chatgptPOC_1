1126e5282cf9aecbbae846b0c58d786e1c8c60fd
Imran Fazli <imr4nfazli@gmail.com>
2023-09-21 15:05:01 +0800

    Wrapped

--- python/openaiAPIConnect.py
+++ python/openaiAPIConnect.py
@@ -1,14 +1,18 @@
 import openai
 
-openai.api_key = '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'
+API_KEY = open('./key.txt', 'r')
+PROMPT_FILE = open('./Prompts/prompt_for_transferring_into_arrayInPython.txt', 'r')
+
+openai.api_key = API_KEY.read()
+PROMPT = PROMPT_FILE.read()
 
 model_engine = "text-davinci-003"
-prompt = "Hello, how are you Robot?"
+prompt = PROMPT
 
 completion = openai.Completion.create(
     engine = model_engine,
     prompt = prompt,
-    max_tokens = 1024,
+    max_tokens = 2024,
     n = 1,
     stop = None,
     temperature = 0.5
